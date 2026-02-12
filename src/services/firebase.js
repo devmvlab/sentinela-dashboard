f14e4d0791b6d03955be906badd9655e8dc9de3c
Matheus Crepaldi <matheus_crepaldi_1991@hotmail.com>
2026-02-11 23:18:17 -0300

    so pegando do ambient

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -5,25 +5,13 @@ import { getStorage } from "firebase/storage";
 
 //PRD
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+	apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+	authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+	projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+	storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+	appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
-
-
-//DEV
-// const firebaseConfig = {
-//   apiKey: "AIzaSyCJv1CMvVjhobMZ6x-1vCHexynI4xGX9hg",
-//   authDomain: "sentinela-app-dev.firebaseapp.com",
-//   projectId: "sentinela-app-dev",
-//   storageBucket: "sentinela-app-dev.firebasestorage.app",
-//   messagingSenderId: "97627603867",
-//   appId: "1:97627603867:web:4f3c8daa0112b78d46d73e",
-//   measurementId: "G-72VSD1MGCB"
-// };
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
