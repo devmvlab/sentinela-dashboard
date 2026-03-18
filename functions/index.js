00fb333c7e37563b5db1d14035734c14aaa526f2
Vainer Fabri <dev.mvlab@gmail.com>
2026-03-17 23:17:35 -0300

    redeploy function antiga

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,5 @@
-const { setGlobalOptions } = require("firebase-functions/v2");
 const { onCall } = require("firebase-functions/v2/https");
+const { setGlobalOptions } = require("firebase-functions/v2");
 const admin = require("firebase-admin");
 
 admin.initializeApp();
@@ -18,71 +18,20 @@ exports.sendPushNotification = onCall(async (request) => {
     throw new Error("cityId é obrigatório");
   }
 
-  // Busca TODOS usuários da cidade
-  const usersSnapshot = await admin.firestore()
-    .collection("users")
-    .where("cityId", "==", cityId)
-    .get();
+  const topic = `sentinela_${cityId}`;
 
-  if (usersSnapshot.empty) {
-    return { success: false, message: "Nenhum usuário encontrado" };
-  }
+  const payload = {
+    data: {
+      title: title,
+      body: message
+    },
+    topic
+  };
 
-  // Monta lista de mensagens
-  const uniqueTokens = new Set();
-
-    usersSnapshot.forEach(doc => {
-      const user = doc.data();
-
-      if (user.tokens && Array.isArray(user.tokens)) {
-        user.tokens.forEach(token => {
-          if (token && token.startsWith("ExponentPushToken")) {
-            uniqueTokens.add(token);
-          }
-        });
-      }
-    });
-
-  const messages = Array.from(uniqueTokens).map(token => ({
-    to: token,
-    sound: "default",
-    title,
-    body: message
-  }));
-
-  if (messages.length === 0) {
-    return { success: false, message: "Nenhum token válido" };
-  }
-
-  // Expo aceita até 100 por request → precisamos dividir
-  const chunkSize = 100;
-  const chunks = [];
-
-  for (let i = 0; i < messages.length; i += chunkSize) {
-    chunks.push(messages.slice(i, i + chunkSize));
-  }
-
-  // Envia todos os lotes
-  const results = [];
-
-  for (const chunk of chunks) {
-    const response = await fetch("https://exp.host/--/api/v2/push/send", {
-      method: "POST",
-      headers: {
-        "Accept": "application/json",
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(chunk)
-    });
-
-    const result = await response.json();
-    results.push(result);
-  }
+  await admin.messaging().send(payload);
 
   return {
     success: true,
-    totalUsers: messages.length,
-    batches: chunks.length,
-    expoResponse: results
+    topic: topic
   };
 });
